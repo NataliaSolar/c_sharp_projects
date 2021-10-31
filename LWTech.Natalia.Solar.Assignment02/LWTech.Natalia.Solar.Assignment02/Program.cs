44a68fb6fbcfd8178821da204452bf08de8dfc21
NataliaSolar <88748524+NataliaSolar@users.noreply.github.com>
2021-10-31 15:42:49 -0600

    C# projects

---
+++ LWTech.Natalia.Solar.Assignment02/LWTech.Natalia.Solar.Assignment02/Program.cs
@@ -0,0 +1,252 @@
+﻿using System;
+
+//CSD 228
+//Assignment02
+//Natlia Solar
+// design reviewer: Chris S Ellis
+
+namespace LWTech.Natalia.Solar.Assignment02
+{
+    enum Players { Human, Computer, NoOne };
+
+    class Program
+    {        
+        static Random rng = new Random();
+        const int humanChecker = 1;
+        const int computerChecker = 2;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connect Four Game");
+            Console.WriteLine("========================================");
+            Console.WriteLine();
+
+            int[,] grid = new int[7,7];
+            bool gameOver = false;
+            Players currentPlayer = Players.Human; ;            
+            int columnNumber = 0;
+            Players winner = Players.NoOne;
+            
+
+            // Initialize the grid
+            InitializeGrid(grid);
+
+
+            // While we don't have a winner...
+            while (!gameOver)
+            {
+                //  Display the grid
+                Console.WriteLine("It's your turn!  Here's the current grid:");
+                DisplayGrid(grid);
+
+                // Have a human take turn
+                currentPlayer = Players.Human;
+
+                //Prompt human for column number                
+                columnNumber =  PromptColumnNumberHuman(grid);
+
+                //Drop the checker into the column
+                DropChecker(grid, columnNumber, currentPlayer);
+
+                // Display the new grid
+                DisplayGrid(grid);
+
+                // Did he get four in a row?
+                if (FoundFourInARow(grid, Players.Human))
+                {
+                    gameOver = true;
+                    winner = Players.Human;
+                    break;
+                }
+                
+                // No. Have a computer take turn                
+                currentPlayer = Players.Computer;
+                Console.WriteLine("It's the computer's turn.");
+
+                columnNumber = PromptColumnNumberComputer(grid);
+                Console.ReadLine();
+
+                Console.WriteLine($"The computer drops a checker in column #{columnNumber + 1}");
+                Console.ReadLine();
+
+                //Drop the checker into the column
+                DropChecker(grid, columnNumber, currentPlayer);
+
+                // Display the grid
+                DisplayGrid(grid);
+                Console.ReadLine();
+
+                // Did the computer get four in a row?
+                if (FoundFourInARow(grid, Players.Computer))
+                {
+                    gameOver = true;
+                    winner = Players.Computer;
+                    break;
+                }
+
+                //No, Is it a Tie game?
+                if (TieGame(grid))
+                {
+                    gameOver = true;
+                    winner = Players.NoOne;                    
+                }
+                else Console.WriteLine("\nNew round!\n");
+            }
+
+            if (winner == Players.Human) Console.WriteLine("Congratulations! You Won!");
+            else if (winner == Players.Computer) Console.WriteLine("Computer won!");
+            else if (winner == Players.NoOne) Console.WriteLine("It's a Tie! Game over...");
+            Console.ReadLine();
+        }
+
+
+
+
+        private static void InitializeGrid(int[,] grid)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    grid[i,j] = 0;
+                }
+            }
+        }
+
+
+
+        private static void DisplayGrid(int[,] grid)
+        {
+            char c;
+            Console.WriteLine("---+---+---+---+---+---+---");
+            for (int j = 0; j < 7; j++)
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    if (grid[i, j] == 1) c = 'X';
+                    else if (grid[i, j] == 2) c = 'O';
+                    else c = ' ';
+                    Console.Write(" " + c);
+                    if (i < 7)
+                        Console.Write(" |");
+                }
+                Console.WriteLine();
+                if (j < 7)
+                    Console.WriteLine("---+---+---+---+---+---+---");
+            }
+            Console.WriteLine();
+        }
+
+
+
+        private static int PromptColumnNumberHuman(int[,] grid)
+        {
+            string input = "";
+            bool gotColumn = false;
+            int columnNumber = 0;
+            do
+            {
+                Console.WriteLine("Which column would you like to drop a checker into (1-7)?");
+                input = Console.ReadLine();
+                while (!int.TryParse(input, out columnNumber) || columnNumber < 1 || columnNumber > 7)
+                { 
+                        Console.WriteLine("I'm sorry. I couldn't understand what you entered.  Please enter column number again.");
+                        input = Console.ReadLine();
+                }                
+                // check if column is not full
+                if (!ColumnIsFull(columnNumber - 1, grid)) gotColumn = true;
+                else
+                {
+                    Console.WriteLine("This column is full.  Please enter different column number again.");
+                    gotColumn = false;
+                }
+            } while (!gotColumn);
+            return columnNumber-1;
+        }
+
+
+        private static bool ColumnIsFull(int column, int[,] grid)
+        {
+            for (int j = 6; j >=0; j--)
+            {
+                if (grid[column, j] == 0) return false;               
+            }
+            return true;
+        }
+
+
+        private static void DropChecker(int[,] grid, int columnNumber, Players currentPlayer)
+        {          
+            int freeSpotIndex = -1;
+            int j = 0;
+            for (j = 0; j<7; j++)
+            {
+                if (grid[columnNumber, j] == 0) freeSpotIndex = j;
+            }
+            if (currentPlayer == Players.Human) grid[columnNumber, freeSpotIndex] = humanChecker;
+            else
+            {
+                grid[columnNumber, freeSpotIndex] = computerChecker;
+            }
+        }
+
+
+        private static int PromptColumnNumberComputer(int[,] grid)
+        {
+            bool gotColumn = false;
+            int columnNumber = 0;
+            while (!gotColumn)
+            {
+                columnNumber = rng.Next(7);
+                if (!ColumnIsFull(columnNumber, grid)) gotColumn = true;
+            }
+            return columnNumber;
+        }
+
+
+        private static bool FoundFourInARow(int[,] grid, Players currentPlayer)
+        {
+            int currentChecker = 0;
+            int counter = 0;
+            if (currentPlayer == Players.Human) currentChecker = humanChecker;
+            else currentChecker = computerChecker;
+            //check vertical
+            for (int i = 0; i<7; i++)
+            {
+                for (int j = 6; j>=0; j--)
+                {
+                    if (grid[i, j] != currentChecker) counter = 0;                   
+                    if (grid[i, j] == 0) break;
+                    if (grid[i, j] == currentChecker) counter++;
+                    if (counter == 4) return true;
+                }
+                counter = 0;
+            }
+            //ckeck horisontal
+            for (int j = 0; j < 7; j++)
+            {
+                for (int i = 6; i >= 0; i--)
+                {
+                    if (grid[i, j] != currentChecker) counter = 0;
+                    if (grid[i, j] == currentChecker) counter++;
+                    if (counter == 4) return true;
+                }
+                counter = 0;
+            }
+            return false;
+
+        }
+
+
+        private static bool TieGame(int[,] grid)
+        {
+            for (int i = 0; i<7; i++)
+            {
+                if (!ColumnIsFull(i, grid)) return false;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ LWTech.Natalia.Solar.Assignment03/LWTech.NataliaSolar.Assignment03/Program.cs
@@ -0,0 +1,246 @@
+﻿using System;
+
+
+namespace LWTech.NataliaSolar.Assignment03
+{
+    public enum Suits { Hearts, Diamonds, Clubs, Spades };
+    public enum Ranks { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King };
+
+    public class Card
+    {
+        public Suits Suit { get; private set; }
+        public Ranks Rank { get; private set; }
+
+
+
+        public Card(Ranks rank = Ranks.Ace, Suits suit = Suits.Hearts)
+        {
+            this.Rank = rank;
+            this.Suit = suit;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Rank} of {Suit}]";
+        }
+
+    }
+
+    public class Deck
+    {
+        public Card[] CardDeck { get; private set; }
+        public int Count { get; private set; }
+        public int DeckSize { get; private set; }
+
+        public Deck()
+        {
+            DeckSize = 52;
+            CardDeck = new Card[DeckSize];
+            Count = 0;
+            foreach (Suits suit in Enum.GetValues(typeof(Suits)))
+            {
+                foreach (Ranks rank in Enum.GetValues(typeof(Ranks)))
+                {
+
+                    CardDeck[Count] = new Card(rank, suit);
+                    Count++;
+                }
+            }
+        }
+
+        public Deck (Deck deck)
+        {
+            this.DeckSize = deck.DeckSize;
+            this.Count = deck.Count;
+            CardDeck = new Card[DeckSize];            
+            for (int i = 0; i < deck.CardDeck.Length; i++ )
+            {
+                this.CardDeck[i] = deck.CardDeck[i];
+            }
+            this.Count = deck.Count;            
+        }
+
+        public Deck (Card[] deckArray)
+        {
+            DeckSize = deckArray.Length;
+            CardDeck = new Card[DeckSize];
+            Count = 0;
+            for (int i = 0; i < DeckSize; i++)
+            {
+                CardDeck[i] = deckArray[i];
+                Count++;
+            }
+ 
+        }
+
+
+        // Fisher-Yates algorithm source: https://exceptionnotfound.net/understanding-the-fisher-yates-card-shuffling-algorithm/
+        public void Shuffle()
+        {
+            Count = 1;
+            Random rng = new Random();
+            for (int i = DeckSize - 1; i > 0; --i)
+            {                
+                int j = rng.Next(i + 1);
+                Card temp = CardDeck[i];
+                CardDeck[i] = CardDeck[j];
+                CardDeck[j] = temp;
+                Count++;
+            }
+        }
+
+
+        public void Cut()
+        {
+            Random rng = new Random();
+            int cutDepth = rng.Next(46)+ 3;
+            Card[] cutDeck = new Card[DeckSize];
+            Count = 0;
+
+            for (int i = cutDepth; i< DeckSize; i++)
+            {
+                cutDeck[Count] = CardDeck[i];
+                Count++;
+            }            
+            for (int i = 0; i < cutDepth; i++)
+            {
+                cutDeck[Count] = CardDeck[i];
+                Count++;
+            }
+            CardDeck = cutDeck;
+        }
+
+        public override string ToString()
+        {
+            string s = "[";
+            for (int i = 0; i< DeckSize; i++ )
+            {
+                s += CardDeck[i].ToString();
+                if (i != DeckSize - 1) s += ", ";
+            }
+            s += $"]\n{Count} cards in deck.";
+            return s;
+        }
+
+
+    }
+
+    public class Player
+    {
+        public Card[] Hand { get; private set; }
+        public int HandSize { get; private set; }//5 cards
+
+        public Player(int handSize = 3)
+        {
+            this.HandSize = handSize;
+            Hand = new Card[HandSize];
+            for (int i = 0; i< HandSize; i++)
+            {
+                Hand[i] = new Card();
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = "[";
+            for (int i = 0; i < HandSize; i++)
+            {
+                s += Hand[i].ToString();
+                if (i != HandSize - 1) s += ", ";
+            }
+            s += "]\n";
+            return s;
+        }
+    }
+
+
+    public class Game
+    {
+        public Deck GameDeck { get; private set; }
+        public int NumberOfPlayers { get; private set; } //4 players
+        public Player[] GamePlayers { get; private set; }
+
+
+        public Game(Deck cardDeck, int numberOfPlayers = 2, int handSize = 3)
+        {
+            this.GameDeck = new Deck(cardDeck);
+            this.NumberOfPlayers = numberOfPlayers;
+            GamePlayers = new Player[NumberOfPlayers];
+            for (int i = 0; i < NumberOfPlayers; i++)
+            {
+                GamePlayers[i] = new Player(handSize);
+            }
+            
+        }
+        
+
+        public Deck DealCards()
+        {
+            int index = GameDeck.DeckSize - 1;
+            int cardLimit = GamePlayers[0].HandSize;
+            for (int card = 0; card < cardLimit; card++)
+            {
+                for (int player = 0; player < NumberOfPlayers; player++)
+                {
+                    GamePlayers[player].Hand[card] = GameDeck.CardDeck[index];
+                    index--;
+                }
+            }
+
+            Card[] newDeckArray = new Card[index+1];
+            for (int i = 0; i <=index; i++)
+            {
+                newDeckArray[i] = GameDeck.CardDeck[i];
+            }
+            Deck newDeck = new Deck(newDeckArray);
+            return newDeck;
+        }
+
+
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < NumberOfPlayers; i++)
+            {
+                s += GamePlayers[i].ToString();
+            }           
+            return s;
+        }
+
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("New deck:");
+            Deck deck = new Deck();
+            Console.WriteLine(deck);
+            Console.ReadLine();
+
+            Console.WriteLine("Shuffled deck:");
+            deck.Shuffle();
+            Console.WriteLine(deck);
+            Console.ReadLine();
+
+            Console.WriteLine("Cut deck:");
+            deck.Cut();
+            Console.WriteLine(deck);
+            Console.ReadLine();
+
+            Console.WriteLine("Dealt hands:");
+            Game game = new Game(deck,4,5);
+            deck = game.DealCards();
+            Console.WriteLine(game);
+            Console.ReadLine();
+
+            Console.WriteLine("Remaining cards in deck:");
+            Console.WriteLine(deck);
+            Console.ReadLine();
+        }
+    }
+}
